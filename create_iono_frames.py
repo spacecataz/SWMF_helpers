f53bc299fa8c9d89c3f455391f92814d3a3942a4
Dan Welling <daniel.welling@uta.edu>
2021-07-07 10:49:10 -0500

    Added option for setting colatitude limit. Cleaned up some of the code a bit.

--- create_iono_frames.py
+++ create_iono_frames.py
@@ -14,11 +14,15 @@ from sys import stdout
 from dateutil.parser import parse
 from argparse import ArgumentParser
 
+# Initialize argument parser:
 parser = ArgumentParser(description=__doc__)
 parser.add_argument("-i", "--imffile", default='',
                     help="Path to relevant SWMF-formatted IMF input file.  "+
                     "Default action is to search in current directory for "+
                     "imf*dat")
+parser.add_argument("-colat", "--colat", type=int, default=40,
+                    help="Set co-latitude plot limit for ionosphere " +
+                    "contour plots.")
 
 # Handle arguments:
 args = parser.parse_args()
@@ -34,8 +38,7 @@ from spacepy.pybats import rim, ImfInput
 # Switch to spacepy's style:
 style()
 
-iefiles = glob('IE/it*.idl')
-
+# Find the IMF file:
 imfpath = glob('./imf*.dat')[0] if not args.imffile else args.imffile
 imf = ImfInput(imfpath)
 
@@ -59,21 +62,23 @@ def create_figure(iefile):
     fig = plt.figure(figsize=(10,10))
     fig.subplots_adjust(left=.08, bottom=.052, top=.95,
                         right=.962, wspace=.217, hspace=.23)
+    # Set common keyword arguments:
+    kwargs = {'max_colat':args.colat, 'target':fig, 'add_cbar':True}
+    
     # Add IE plots:
     # FACs:
     with plt.style.context('default'):
         zmax = max(np.abs(ie['n_jr']).max(), np.abs(ie['s_jr']).max())
-        ie.add_cont('n_jr', target=fig, loc=431, add_cbar=True)
-        ie.add_cont('s_jr', target=fig, loc=434, add_cbar=True, label='')
+        ie.add_cont('n_jr', loc=431, **kwargs)
+        ie.add_cont('s_jr', loc=434, label='', **kwargs)
         # J_phi:
         zmax = max(np.abs(ie['n_jphi']).max(), np.abs(ie['s_jphi']).max())
-        ie.add_cont('n_jphi', target=fig, loc=432, add_cbar=True)
-        ie.add_cont('s_jphi', target=fig, loc=435, add_cbar=True, label='')
+        ie.add_cont('n_jphi', loc=432, **kwargs)
+        ie.add_cont('s_jphi', loc=435, label='', **kwargs)
         # J_phi:
         zmax = max(np.abs(ie['n_phi']).max(), np.abs(ie['s_phi']).max())
-        ie.add_cont('n_phi', target=fig, loc=433, add_cbar=True)
-        ie.add_cont('s_phi', target=fig, loc=436, add_cbar=True, label='')
-
+        ie.add_cont('n_phi', loc=433, **kwargs)
+        ie.add_cont('s_phi', loc=436, label='', **kwargs)
     
     a4 = fig.add_subplot(413)
     a5 = fig.add_subplot(414, sharex=a4)
