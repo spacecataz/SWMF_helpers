2775995121f2ff72f0a39968d6654f5f3163672b
Dan Welling <daniel.welling@uta.edu>
2022-06-01 14:02:51 -0500

    Linting, removing python2 'rawinput' stuff

--- CatLog.py
+++ CatLog.py
@@ -6,7 +6,7 @@ lapping points.  It assumes that the first column is either time elapsed or
 total run iteration.  If any file does not have the same header as the first,
 it is disregarded.
 
-Disclaimer: Always check your appended files.  Incomplete files, extra 
+Disclaimer: Always check your appended files.  Incomplete files, extra
 newlines, and mismatched files can yield unexpected results.
 
 Usage: CatLog.py [options] log1 log2 [log3] [log4]...[logN]
@@ -19,38 +19,33 @@ Examples:
     1) Append log_0002.log to log_0001.log, do not check for duplicate lines.
        >CatLog.py -nocheck log_0001.log log_0002.log
 
-    2) Combine all files that begin with 'sat_cluster_n' and remove all but one:
+    2) Combine all files that begin with 'sat_cluster_n' and remove all
+       but one:
        >CatLog.py -rm sat_cluster*
 
     3) Combine all geoindex files into a new, single file:
        > CatLog.py -o=geoindex_all.log geoindex*.log
 '''
 
-import sys
 import re
 from os import unlink
 from shutil import copy
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-# Set raw input command based on python version:
-if sys.version_info[0] == 3:
-    rawinput = input
-else:
-    rawinput = raw_input
 
 def get_time(line, index, debug=False):
     '''
-    From a string entry, return the "time" the file was written.  This is 
-    done by splitting the line and taking all items corresponding to the 
-    indexes within the input list "index", concatenating them, and 
+    From a string entry, return the "time" the file was written.  This is
+    done by splitting the line and taking all items corresponding to the
+    indexes within the input list "index", concatenating them, and
     converting the resulting line into a float.  Many preceeding zeros are
     added to each entry to compensate for frequenty chances in within
     log files.
     '''
 
     parts = line.split()
-    keep  = ''
-    
+    keep = ''
+
     for i in index:
         keep += '{:0>2}'.format(parts[i])
 
@@ -60,13 +55,14 @@ def get_time(line, index, debug=False):
         print('Reducing to {}'.format(keep))
 
     return int(keep)
-    
+
+
 # Create argument parser & set up arguments:
 parser = ArgumentParser(description=__doc__,
                         formatter_class=RawDescriptionHelpFormatter)
-parser.add_argument("-o", "--outfile", default=None, help="Rather than append "+
-                    "to first file, create new file to store output.")
-parser.add_argument("files", nargs='+', help="Files to convert.  Can be "+
+parser.add_argument("-o", "--outfile", default=None, help="Rather than append" +
+                    " to first file, create new file to store output.")
+parser.add_argument("files", nargs='+', help="Files to convert.  Can be " +
                     "explicit files or a unix wildcard.")
 parser.add_argument("-rm", "--remove", action="store_true",
                     help="Remove all but the first file given.")
@@ -81,7 +77,7 @@ args = parser.parse_args()
 # Re-order files as the standard unix order will not reflect the
 # true order for very large iteration runs.
 pattern = '[_\-ned]{0,2}(\d+)[_\-ned]{0,2}(\d+)?\.[(log)|(sat)|(mag)]'
-if len(args.files)> 1:
+if len(args.files) > 1:
     # Get all file iterations.
     iters = {}
     for f in args.files:
@@ -101,7 +97,7 @@ if args.debug:
     print("File order = \n")
     for f in args.files:
         print("\t{}\n".format(f))
-    if rawinput('Is this correct? [y/N]') != 'y': 
+    if input('Is this correct? [y/N]') != 'y':
         raise Exception('Ordering error.')
 
 # Create new file if "outname" is set:
@@ -113,24 +109,25 @@ else:
     args.outfile = args.files.pop(0)
 
 # Open output file in append mode:
-out=open(args.outfile, 'a+')
+out = open(args.outfile, 'a+')
 
 # Some machines don't do 'a+' correctly.  Rewind file as necessary.
-if out.tell()>0: out.seek(0,0)
+if out.tell() > 0:
+    out.seek(0, 0)
 
 # Load and store header:
-out.readline() #garbage
-head=out.readline() # Header.
-nbytes=len(out.readline())
+out.readline()  # garbage
+head = out.readline()  # Header.
+nbytes = len(out.readline())
 
 # Using header info, create list of indexes corresponding to time.
-time_locs = [] # List of indices
-time_vars = [] # List of variable names (for debugging).
+time_locs = []  # List of indices
+time_vars = []  # List of variable names (for debugging).
 # Desired time variable names in order:
 search_names = ['year', 'yyyy', 'yy', 'yr', 'doy', 'mo', 'month', 'mm', 'day',
                 'dy', 'hour', 'hr', 'hh', 'mm', 'mn', 'min', 'ss', 'sec', 'sc']
 
-iter_names   = ['iter', 'it', 'nstep']
+iter_names = ['iter', 'it', 'nstep']
 for i, part in enumerate(head.split()):
     for s in search_names:
         if s == part.lower():
@@ -146,7 +143,8 @@ if not time_locs:
                 time_locs.append(i)
                 time_vars.append(s)
                 break
-        if time_locs: break # Only want a single iteration tag.
+        if time_locs:
+            break  # Only want a single iteration tag.
 
 # If nothing was found still, default to first column:
 if not time_locs:
@@ -159,11 +157,10 @@ if args.debug:
     print("\tHeader has {} entries." .format(len(head.split())))
     print("\tUsing the following columns in order for time calculation:")
     for i, s in zip(time_locs, time_vars):
-        print("\t[{:02d}] {}".format(i,s))
-    
+        print("\t[{:02d}] {}".format(i, s))
+
 
 # Load last line of file.
-#out.seek(-1*nbytes, 2) #seek last line. THIS USED TO WORK IN PYTHON2.
 last_line = out.readlines()[-1]
 # Get last time entry:
 lasttime = get_time(last_line, time_locs, args.debug)
@@ -174,40 +171,47 @@ if args.debug:
 # Open rest of files, append.
 for f in args.files:
     # No files that end with special characters.
-    if f[-1]=='~': continue
+    if f[-1] == '~':
+        continue
     # Open file, slurp lines.
-    if args.debug: print("Processing {}:".format(f))
-    nextfile=open(f, 'r')
-    lines=nextfile.readlines()
+    if args.debug:
+        print("Processing {}:".format(f))
+    nextfile = open(f, 'r')
+    lines = nextfile.readlines()
     nextfile.close()
     # Read header; skip this file if header is different.
     lines.pop(0)
-    nowhead=lines.pop(0)
-    if nowhead!=head: 
-        if args.debug: 
+    nowhead = lines.pop(0)
+    if nowhead != head:
+        if args.debug:
             print(head)
             print(nowhead)
             print("\tHeader does not match, discarding.")
         continue
     # Jump over overlapping lines:
     if not args.nocheck:
-        nSkip=0; nLines=len(lines)
-        while nSkip<nLines:
+        nSkip = 0
+        nLines = len(lines)
+        while nSkip < nLines:
             if get_time(lines[0], time_locs) > lasttime:
                 break
             else:
                 lines.pop(0)
-                nSkip+=1
-        if args.debug: print("\tFound {} overlapping lines.".format(nSkip))
+                nSkip += 1
+        if args.debug:
+            print("\tFound {} overlapping lines.".format(nSkip))
     # Append data to first log.
-    if len(lines)<1: continue
+    if len(lines) < 1:
+        continue
     for l in lines:
         out.write(l)
     # Save "last time".
-    if not args.nocheck: lasttime=get_time(lines[-1], time_locs)
+    if not args.nocheck:
+        lasttime = get_time(lines[-1], time_locs)
     # Delete file when done.
-    if args.remove: 
-        if args.debug: print("\tRemoving file.")
+    if args.remove:
+        if args.debug:
+            print("\tRemoving file.")
         unlink(f)
 
 # Finalize:
