a07917eeeebadf3ad5a784466e41b31123cd32e5
Dan Welling <dwelling@umich.edu>
2025-05-15 12:14:55 -0400

    Added more attributes to output file. Added option for output file name specification Now checking coordinate system.

--- process_sme.py
+++ process_sme.py
@@ -4,9 +4,20 @@
 Given a SWMF magnetometer grid file, calculate SuperMag-like indexes and save
 to file. If the --fetch flag is set, fetch matching indexes from SuperMag
 and save along side SWMF values.
+
+The result is written to JSON-headed ASCII; the default file name is
+'sm_indexes.txt'. The file can be read using spacepy as follows:
+
+```
+from spacepy import datamodel
+data = datamodel.readJSONheadedASCII('sm_indexes.txt')
+```
+
 '''
 
+import re
 import datetime as dt
+import warnings
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
 import numpy as np
@@ -19,6 +30,8 @@ parser = ArgumentParser(description=__doc__,
                         formatter_class=RawDescriptionHelpFormatter)
 parser.add_argument('magfile', type=str, help='Name of the magnetometer ' +
                     'grid file to process in to SM indexes.')
+parser.add_argument("-o", "--outfile", default='sm_indexes.txt', help="Set " +
+                    "output file name.  Defaults to 'sm_indexes.txt'")
 
 # Handle arguments:
 args = parser.parse_args()
@@ -34,11 +47,27 @@ tstart, tend = mags.attrs['time_range'][0], mags.attrs['time_range'][1]
 data = SpaceData()
 data.attrs['descrip'] = 'Supermag-like indexes from SWMF MagGrid files.'
 
+# Add attributes to data object based on magnetometer grid input file:
+data.attrs['header'] = mags.attrs['header']
+# stash max/min lat in file as an attribute!
+
+# Extract coordinate system for magnetometer location:
+match = re.search('\((\w{3})\)', mags.attrs['header'])
+data.attrs['coord'] = match.groups()[0]
+
+# Warn if we are not in SMG coordinates.
+# NOTE: For future use, be prepared to transform GEO or MAG to SMG.
+if data.attrs['coord'] != 'SMG':
+    warnings.warn('Input file not in SMG coordinates ' +
+                  f'(coords={data.attrs["coord"]})')
+
+# CHECK MAX/MIN LAT HERE! WARN AS NECESSARY!
+
 # Add time:
 data['time'] = mags.attrs['times']
 
 # Create empty arrays with units:
-varnames = ['SWMFL', 'SWMFU', 'lat_L', 'lat_U', 'lon_L', 'lon_U']
+varnames = ['SWMFL', 'SWMFU', 'mlat_L', 'mlat_U', 'lon_L', 'lon_U']
 for v in varnames:
     data[v] = dmarray(np.zeros(nframe))
     if 'SWMF' in v:
@@ -63,5 +92,15 @@ for i in range(nframe):
 
     data['lon_L'][i] = mags['Lon'][ind_min[0]]
     data['lon_U'][i] = mags['Lon'][ind_max[0]]
-    data['lat_L'][i] = mags['Lat'][ind_min[1]]
-    data['lat_U'][i] = mags['Lat'][ind_max[1]]
+    data['mlat_L'][i] = mags['Lat'][ind_min[1]]
+    data['mlat_U'][i] = mags['Lat'][ind_max[1]]
+
+# Convert SM longitude to MLT:
+data['mlt_L'][i] = dmarray(data['lon_L'][i] / 15. + 12., {'units': 'Hours'})
+data['mlt_U'][i] = dmarray(data['lon_U'][i] / 15. + 12., {'units': 'Hours'})
+
+# GRAB DATA FROM SUPERMAGAPI HERE!
+# Store in `data`!
+
+# Write output file to disk.
+data.toJSONheadedASCII(args.outfile)
